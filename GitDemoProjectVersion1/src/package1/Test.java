63e7080e4b47cf1d3a038b48a8d1e9fc0c816227
Platinum <Platinum@DESKTOP-HR9PAA3>
2020-08-27 15:14:38 +0530

    First Commit

---
+++ GitDemoProjectVersion1/src/package1/Test.java
@@ -0,0 +1,20 @@
+package package1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry="yes";
+		if (hungry=="yes") {
+			System.out.println("Have something ");
+		}
+		else
+		{
+			System.out.println("Do something ");
+		}
+		
+		
+	}
+
+}
